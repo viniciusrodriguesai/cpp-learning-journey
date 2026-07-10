369851670385abf54f36cef94c465fc81c81f787
Vinicius Mangueira <viniciusmangueira04@gmail.com>
2026-07-10 17:02:06 -0300

    Terminando o codigo de escopos e operadores

--- 01-fundamentos/escopo_operadores.cpp
+++ 01-fundamentos/escopo_operadores.cpp
@@ -8,16 +8,19 @@ int main(){
 
 
     int n3,n4; //Variaveis locais
-    int res;
+    int res1,res2;
 
     n1 = 11;
     n2 =3;
     n3 = 5;
     n4 = 2;
 
-    res = n1 + n2 + n3 + n4;
+    res1 = n1/n2;
+    res2 = n1%n2;
 
-    std::cout << "Soma de todas as variaveis:" << res << "\n\n";
+    std::cout << "Divisao:" << res1 << "\n";
+    std::cout << "Resto:" << res2 << "\n\n";
+
 
     return 0;
 
