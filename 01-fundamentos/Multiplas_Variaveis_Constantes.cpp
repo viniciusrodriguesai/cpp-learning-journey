cf2bccdee06b6745d8126db19afc8151608950ff
Vinicius Mangueira <viniciusmangueira04@gmail.com>
2026-07-10 16:34:17 -0300

    Apredendo variaveis

---
+++ 01-fundamentos/Multiplas_Variaveis_Constantes.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#define pi 3.1415
+#define canal std::cout << "Vinicius" << "\n\n";
+
+int main(){
+
+    int vidas=3,tiros = 500 ,life = 100;
+    canal
+    std::cout << pi << "\n";
+
+    return 0;
+}
