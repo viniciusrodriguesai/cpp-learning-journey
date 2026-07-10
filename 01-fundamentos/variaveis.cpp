3fb1ae300cb7fc42a53322a52f7240eae1b28461
Vinicius Mangueira <viniciusmangueira04@gmail.com>
2026-07-10 16:16:16 -0300

    Mostrando na tela as variaveis

--- 01-fundamentos/variaveis.cpp
+++ 01-fundamentos/variaveis.cpp
@@ -4,11 +4,21 @@ using namespace std;
 
 int main(){
 
-    int vidas;
-    char letra;
-    double decimal;
-    float decimal;
-    bool vivo;
+    int vidas=0;
+    char letra='b';
+    double decimal=5.2;
+    float decimal2=5.2;
+    bool vivo=true;
+    string nome="Vinicius";
 
-    return 0
+    cout << vidas << endl;
+    cout << letra << endl;
+    cout << decimal << endl;
+    cout << decimal2 << endl;
+    cout << vivo << endl;
+    cout << nome << endl;
+
+
+
+    return 0;
 }
