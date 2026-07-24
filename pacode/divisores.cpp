957b42a6dd7143f84b4edf8d4e8578667f5e069d
Vinicius Mangueira <viniciusmangueira04@gmail.com>
2026-07-24 09:10:33 -0300

    Fazendo os codigos da aula

---
+++ pacode/divisores.cpp
@@ -0,0 +1,11 @@
+using namespace std;
+ll = long long
+
+
+int main(){
+    int n;
+    cin >>n;
+
+    vector<int>
+
+}
---
+++ pacode/progamação_dinamica(intermediario)/fatoracao.cpp
@@ -0,0 +1,11 @@
+#include <ionstream>
+
+int main(){
+
+    for(int i = 2; i * 1 <= n; i++){
+    if(n % i == 0){
+        int = a = 0;
+        while}
+    }
+
+}
