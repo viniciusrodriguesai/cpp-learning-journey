f539b3db6693338f48a192d9625a0d53596c7c00
Vinicius Mangueira <viniciusmangueira04@gmail.com>
2026-07-24 15:54:13 -0300

    feat: solve factorial gcd problem

--- pacode/Number_Theory_(Iniciante)/bored_with_life.cpp
+++ pacode/Number_Theory_(Iniciante)/bored_with_life.cpp
@@ -5,5 +5,14 @@ int main() {
     long long a, b;
     cin >> a >> b;
 
+    long long x = min(a, b);
+    long long fat = 1;
+
+    for (long long i = 1; i <= x; i++) {
+        fat *= i;
+    }
+
+    cout << fat << endl;
+
     return 0;
 }
