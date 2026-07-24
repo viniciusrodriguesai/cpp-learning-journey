9b37ddf6e6c289c8806db68c388882a60fd198ea
Vinicius Mangueira <viniciusmangueira04@gmail.com>
2026-07-24 16:51:02 -0300

    feat: search next prime number

--- pacode/Number_Theory_(Iniciante)/next_prime.cpp
+++ pacode/Number_Theory_(Iniciante)/next_prime.cpp
@@ -3,11 +3,21 @@ using namespace std;
 
 bool isPrime(int n) {
     if (n <= 1) return false;
+
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0) return false;
+    }
+
+    return true;
 }
 
 int main() {
     int x;
     cin >> x;
+
+    while (!isPrime(x)) {
+        x++;
+    }
 
     return 0;
 }
