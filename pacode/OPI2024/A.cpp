985a6f5357a34e3d656ecc949296276910148d10
Vinicius Mangueira <viniciusmangueira04@gmail.com>
2026-08-07 09:58:58 -0300

    Add OPI 2024 problem A materials
    Include the official PDF statement for problem A.
    
    Add the initial C++ solution scaffold with a 64-bit integer alias, precomputed powers of ten, and the Card data structure.

---
+++ pacode/OPI2024/A.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+ll pow10[7] = {1, 10, 100, 1000, 10000, 100000, 1000000};
+struct Card {
+    int val, len;
+};
+int main() {
+
+}
