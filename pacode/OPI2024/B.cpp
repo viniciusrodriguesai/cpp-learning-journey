08fdafcad5c899b54e1dea57c7a0740b3acaef3e
Vinicius Mangueira <viniciusmangueira04@gmail.com>
2026-08-07 09:19:29 -0300

    Adiciona estrutura inicial do programa B

--- pacode/OPI2024/B.cpp
+++ pacode/OPI2024/B.cpp
@@ -1,3 +1,8 @@
 #include <bits/stdc++.h>
 #include <bits/stdc++.h>
 using namespace std;
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);;
+    int N, K;
