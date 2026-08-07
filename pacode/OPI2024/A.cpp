40732f78981a8b7be9a72e332ea472d8b903e496
Vinicius Mangueira <viniciusmangueira04@gmail.com>
2026-08-07 10:19:13 -0300

    Restore string concatenation solution for problem A
    Read the input values as strings, order them by length and lexicographic value, and apply the concatenation comparator to maximize the resulting number.
    
    Keep the unrelated problem H files untracked and outside this commit.

--- pacode/OPI2024/A.cpp
+++ pacode/OPI2024/A.cpp
@@ -5,97 +5,18 @@ int main() {
     cin.tie(nullptr);
     int N;
     cin >> N;
-    vector<vector<int>> a(N, vector<int>(N, 0));
-    vector<bool> used(N * N + 1, false);
-    auto put = [&](int i, int j, int x) {
-        a[i][j] = x;
-        used[x] = true;
-    };
-    if (N % 2 == 0) {
-        int k = N / 2;
-        int C = 2 * N + 1;
-        while (C % 6 != 3)
-            C++;
-        for (int j = 0; j < N; j++) {
-            int odd = 2 * j + 1;
-            int even = C - odd;
-            put(k - 1, j, odd);
-            put(k, j, even);
-        }
-        vector<int> odds, evens;
-        for (int x = 1; x <= N * N; x++) {
-            if (used[x]) continue;
-            if (x % 2)
-                odds.push_back(x);
-            else
-                evens.push_back(x);
-        }
-        int po = 0, pe = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (a[i][j] != 0)
-                    continue;
-                if (i < k)
-                    put(i, j, odds[po++]);
-                else
-                    put(i, j, evens[pe++]);
-            }
-        }
-    } else {
-        int k = N / 2;
-        put(k - 1, k + 1, 1);
-        put(k, k + 1, 8);
-        put(k, k, 7);
-        put(k + 1, k, 2);
-        int t = 0;
-        for (int j = k + 2; j < N; j++) {
-            int odd = 6 * t + 3;
-            int even = 6 * t + 6;
-            put(k - 1, j, odd);
-            put(k, j, even);
-            t++;
-        }
-        for (int j = 0; j < k; j++) {
-            int odd = 6 * t + 3;
-            int even = 6 * t + 6;
-            put(k, j, odd);
-            put(k + 1, j, even);
-            t++;
-        }
-        vector<int> odds, evens;
-        for (int x = 1; x <= N * N; x++) {
-            if (used[x]) continue;
-            if (x % 2)
-                odds.push_back(x);
-            else
-                evens.push_back(x);
-        }
-        int po = 0, pe = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (a[i][j] != 0)
-                    continue;
-                bool oddRegion;
-                if (i < k)
-                    oddRegion = true;
-                else if (i > k)
-                    oddRegion = false;
-                else
-                    oddRegion = (j <= k);
-                if (oddRegion)
-                    put(i, j, odds[po++]);
-                else
-                    put(i, j, evens[pe++]);
-            }
-        }
-    }
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            if (j)
-                cout << ' ';
-            cout << a[i][j];
-        }
-        cout << '\n';
-    }
+    vector<string> a(N);
+    for (int i = 0; i < N; i++)
+        cin >> a[i];
+    sort(a.begin(), a.end(), [](const string &x, const string &y) {
+        if (x.size() != y.size())
+            return x.size() > y.size();
+        return x > y;
+    });
+    vector<string> v = {a[0], a[1], a[2]};
+    sort(v.begin(), v.end(), [](const string &x, const string &y) {
+        return x + y > y + x;
+    });
+    cout << v[0] << v[1] << v[2] << '\n';
     return 0;
 }
