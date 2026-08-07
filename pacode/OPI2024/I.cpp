862cac7a226df6b7d776e813508a5a3497ed83a8
Vinicius Mangueira <viniciusmangueira04@gmail.com>
2026-08-07 10:30:36 -0300

    Initialize bipartite traversal for problem I
    Build the undirected adjacency list from the tree input and initialize breadth-first traversal from node 1.
    
    Add the color array and partition counters required for the upcoming bipartite component analysis.

--- pacode/OPI2024/I.cpp
+++ pacode/OPI2024/I.cpp
@@ -1,5 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    vector<vector<int>> g(n + 1);
+    for (int i = 0; i < n - 1; i++) {
+        int u, v;
+        cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+    vector<int> cor(n + 1, -1);
+    long long cnt[2] = {0, 0};
+    queue<int> q;
+    cor[1] = 0;
+    q.push(1);
 }
