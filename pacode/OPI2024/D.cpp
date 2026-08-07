eb39c9d6fa4858eeff4ed8d4f11ff96de10bb40e
Vinicius Mangueira <viniciusmangueira04@gmail.com>
2026-08-07 09:43:12 -0300

    Read input and detect odd values

--- pacode/OPI2024/D.cpp
+++ pacode/OPI2024/D.cpp
@@ -1,5 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    bool tem_impar = false;
+    for (int i = 0; i < n; ++i) {
+        int x;
+        cin >> x;
+        if (x & 1) tem_impar = true;
+    }
 
 }
+
