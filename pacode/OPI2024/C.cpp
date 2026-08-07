b1d2298f27bb9f8ddec0a11f43ceffbf64261079
Vinicius Mangueira <viniciusmangueira04@gmail.com>
2026-08-07 09:26:57 -0300

    Initialize input handling for problem C

--- pacode/OPI2024/C.cpp
+++ pacode/OPI2024/C.cpp
@@ -1,5 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
+        ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int N;
+    cin >> N;
+    unordered_map<string, int> freq;
 
 }
